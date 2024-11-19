9c8b3b107447f3793b55bf86b0a3b2435eb3914f
trang229 <37693520+trang229@users.noreply.github.com>
2024-11-19 22:47:30 +0000

    update

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def add():
 
   return '{"Result":"Success"}'
 
-@app.route("/") #Default - Show Data
+@app.route("/index.html") #Default - Show Data
 def index(): # Name of the method
   return render_template('index.html')
 
